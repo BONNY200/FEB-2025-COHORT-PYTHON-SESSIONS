87944bab5cd9b6ee593c1a9b8e5164f9e7f2ff8e
BONFACE MATOKE <matokebonface4@gmail.com>
2025-03-17 08:26:04 +0300

    first commit

---
+++ PYTHON SESSIONS/WEEK 1/index.py
@@ -0,0 +1,8 @@
+print("hello world")
+number = 10
+print(number)
+
+age = 27 #integer datatype
+marks = 90.6 #float datatype
+
+name = "Bonface" #string datatype
---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+student_info = {"name": "Bonnie", "age": 21, "grade": "A"}
+
+# Accessing a value by its key
+print(student_info["name"])  # Outputs: Bonnie
+
+# Modifying a value
+student_info["age"] = 22
+print(student_info)  # Outputs: {'name': 'Bonnie', 'age': 22, 'grade': 'A'}
